86a97e315c17dd6e502702570b02b4f9d2c80f7e
ZeJsters <zoher.jetpurwala@gmail.com>
2025-02-02 13:08:08 +0530

    State removed from stratergy

--- src/config/passport.js
+++ src/config/passport.js
@@ -18,27 +18,26 @@ export const configurePassport = (passport) => {
         callbackURL: `${process.env.BASE_URL}/auth/twitter/callback`,
         includeEmail: true,
         passReqToCallback: true,
-        state: true,
       },
       (req, accessToken, refreshToken, profile, done) => {
-        console.log("Twitter authentication attempt", {
-          accessToken,
-          refreshToken,
-          profile,
-        });
-
-        if (!profile) {
-          console.error("Twitter authentication failed: No profile returned");
-          return done(null, false, {
-            message: "Twitter authentication failed",
+        if (process.env.NODE_ENV !== 'production') {
+          console.log("Twitter authentication attempt", {
+            accessToken,
+            refreshToken,
+            profile,
           });
         }
 
+        if (!profile) {
+          console.error("Twitter authentication failed: No profile returned");
+          return done(null, false, { message: "Twitter authentication failed" });
+        }
+
         const userData = {
-          id: profile._json.id,
-          name: profile._json.name,
-          username: profile._json.username,
-          image: profile._json.profile_image_url,
+          id: profile._json.id || null,
+          name: profile._json.name || 'Unnamed',
+          username: profile._json.username || 'NoUsername',
+          image: profile._json.profile_image_url || 'default_image_url',
         };
         console.log(userData);
         return done(null, userData);
